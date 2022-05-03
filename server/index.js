01448108123ed3da7475678b01137a657da9e7ac
unknown <Administrator@HP-WKS-060.gmf-aeroasia.co.id>
2022-05-03 19:37:57 +0700

    add server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use("/post", postRoutes)
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://faisalibal:Restvet135@cluster0.njpdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
+    .catch((error)=> console.log(error , "error ni bro"));
+
+mongoose.set('useFindAndModify', false)
